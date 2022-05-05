a3d2777e2422e33574bc298cf829bdda114d6c6a
David <depch47@gmail.com>
2022-05-05 06:57:21 -0500

    IMPLEMENTED react hook form into the CategorForm

--- src/components/CategoryForm.tsx
+++ src/components/CategoryForm.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 interface Props {}
 
+type userInput = { title: string }
+
 const CategoryForm: React.FunctionComponent<Props> = () => {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {}
+  const MIN_LENGTH = 3
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<userInput>()
+  const onSubmit: SubmitHandler<userInput> = (data) => console.log(data)
+  console.log(errors)
+
   return (
-    <form className='w-full max-w-sm p-2' onSubmit={(e) => onSubmit(e)}>
+    <form className='w-full max-w-sm p-2' onSubmit={handleSubmit(onSubmit)}>
       <div className='md:flex md:items-center mb-6'>
         <div className='md:w-1/3'>
           <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='inline-full-name'>
@@ -14,11 +26,14 @@ const CategoryForm: React.FunctionComponent<Props> = () => {
         </div>
         <div className='md:w-2/3'>
           <input
+            {...register('title', { required: true, minLength: MIN_LENGTH })}
             placeholder='to-do category'
             className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500'
             id='inline-full-name'
             type='text'
           />
+          {errors.title?.type === 'required' && <p>This field is required</p>}
+          {errors.title?.type === 'minLength' && <p>Category name must be at least {MIN_LENGTH} characters long</p>}
         </div>
       </div>
       <div className='flex md:items-center'>
