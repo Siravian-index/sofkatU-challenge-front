7eab06d02cd9c40c07a6e6d0788fefd82f6a7161
David <depch47@gmail.com>
2022-05-05 06:48:09 -0500

    INSTALLED react-hook-form dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CategoryForm from './components/CategoryForm'
 import CategoryList from './components/CategoryList'
 
 import Title from './components/Title'
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className=''>
       <ContextProvider>
+        <CategoryForm />
         <CategoryList />
       </ContextProvider>
     </div>
--- src/components/CategoryForm.tsx
+++ src/components/CategoryForm.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react'
 
-interface ICategoryFormProps {}
+interface Props {}
 
-const CategoryForm: React.FunctionComponent<ICategoryFormProps> = (props) => {
+const CategoryForm: React.FunctionComponent<Props> = () => {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {}
   return (
     <form className='w-full max-w-sm p-2' onSubmit={(e) => onSubmit(e)}>
       <div className='md:flex md:items-center mb-6'>
         <div className='md:w-1/3'>
           <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='inline-full-name'>
-            Title:
+            New:
           </label>
         </div>
         <div className='md:w-2/3'>
           <input
+            placeholder='to-do category'
             className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500'
             id='inline-full-name'
             type='text'
           />
         </div>
       </div>
-
       <div className='flex md:items-center'>
         <div className='w-2/3 md:w-1/3'></div>
         <div className='w-1/3 md:w-2/3'>
