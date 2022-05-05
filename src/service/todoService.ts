a99ca9e1071883e17e9ac6104e2cbfd1cb5e1f9c
David <depch47@gmail.com>
2022-05-05 14:47:17 -0500

    REFACTOR---backend now sends a Category instead of a Todo, updated logic to match that change

--- src/service/todoService.ts
+++ src/service/todoService.ts
@@ -1,4 +1,4 @@
-import { HTTP_METHODS, todo, todoWithoutId } from '../types'
+import { category, HTTP_METHODS, todo, todoWithoutId } from '../types'
 
 const ENDPOINT = 'http://localhost:8080/api/v1/todos'
 
@@ -7,14 +7,14 @@ const HEADERS = {
   'Content-Type': 'application/json',
 }
 
-export const createTodo = async (data: todoWithoutId): Promise<todo> => {
+export const createTodo = async (data: todoWithoutId): Promise<category> => {
   const res = await fetch(ENDPOINT, {
     method: HTTP_METHODS.POST,
     headers: HEADERS,
     body: JSON.stringify(data),
   })
-  const todo: todo = await res.json()
-  return todo
+  const category: category = await res.json()
+  return category
 }
 
 export const updateTodo = async (todo: todo): Promise<todo> => {
--- src/stateManagement/reducer.ts
+++ src/stateManagement/reducer.ts
@@ -13,31 +13,20 @@ function reducer(state: categoryList, action: actionType): categoryList {
     case stateAction.DELETE_CATEGORY:
       return state.filter((c) => c.id !== category.id)
     case stateAction.ADD_TODO:
-      const parentCategory = state.find((c) => c.id === todo.categoryFK)
-      console.log('fresh todo', todo)
-
-      if (parentCategory) {
-        const updatedTodoList = [...parentCategory.todoList, todo]
-        return state.map((c) => (c.id === parentCategory.id ? { ...parentCategory, todoList: updatedTodoList } : c))
-      }
-      console.log('should not hit this line')
-      return state
+      return state.map((c) => (c.id === category.id ? { ...category } : c))
     case stateAction.REMOVE_TODO:
       const parent = state.find((c) => c.id === todo.categoryFK)
-      console.log('removing todo', todo)
       if (parent) {
         const updatedList = parent.todoList.filter((t) => t.id !== todo.id)
         return state.map((c) => (c.id === parent.id ? { ...parent, todoList: updatedList } : c))
       }
-      console.log('should not hit this line')
       return state
     case stateAction.UPDATE_TODO:
-      const parentC = state.find((c) => c.id === todo.categoryFK)
-      if (parentC) {
-        const updatedArray = parentC.todoList.map((t) => (t.id === todo.id ? { ...todo } : t))
-        return state.map((c) => (c.id === parentC.id ? { ...parentC, todoList: updatedArray } : c))
+      const parentCategory = state.find((c) => c.id === todo.categoryFK)
+      if (parentCategory) {
+        const updatedArray = parentCategory.todoList.map((t) => (t.id === todo.id ? { ...todo } : t))
+        return state.map((c) => (c.id === parentCategory.id ? { ...parentCategory, todoList: updatedArray } : c))
       }
-      console.log('should not hit this line')
       return state
     default:
       throw new Error('Illegal stateAction passed')
