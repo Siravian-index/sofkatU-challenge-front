8770464a22e6fbaa799aa346b7442cb54a76d9aa
David <depch47@gmail.com>
2022-05-06 08:20:41 -0500

    ADD---clear form error message on click

--- src/components/CategoryForm.tsx
+++ src/components/CategoryForm.tsx
@@ -12,6 +12,7 @@ const CategoryForm: React.FunctionComponent<Props> = () => {
   const {
     register,
     handleSubmit,
+    clearErrors,
     reset,
     formState: { errors },
   } = useForm<userInput>({ mode: 'onSubmit', reValidateMode: 'onSubmit' })
@@ -23,7 +24,7 @@ const CategoryForm: React.FunctionComponent<Props> = () => {
 
   return (
     <form className='w-full max-w-sm p-2' onSubmit={handleSubmit(onSubmit)}>
-      <div className='md:flex md:items-center mb-6'>
+      <div className='md:flex md:items-center mb-6' onClick={() => clearErrors()}>
         <div className='md:w-1/3'>
           <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='inline-full-name'>
             New:
